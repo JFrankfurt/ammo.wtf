86289bef7bdc5fc80c90738a1ae557c0e5996b23
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2025-04-20 23:36:53 -0500

    Updates

--- assets/GravitationalCubes-yMiCKBld.js
+++ assets/GravitationalCubes-yMiCKBld.js
@@ -1,4 +1,4 @@
-import{R as eI,r as XJ,g as zJ,a as NA,j as gg}from"./vendor-react-query-BZoSzIJu.js";import{x as $J,_ as AK}from"./vendor-web3-DyILjG9k.js";import{r as Qk}from"./index-B5mb2KWT.js";/**
+import{R as eI,r as XJ,g as zJ,a as NA,j as gg}from"./vendor-react-query-BZoSzIJu.js";import{x as $J,_ as AK}from"./vendor-web3-DyILjG9k.js";import{r as Qk}from"./index-C2LLdhFp.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
